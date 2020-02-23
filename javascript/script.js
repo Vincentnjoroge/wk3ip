50ef1485e0602e156641c5445900fe2e2eb2d736
vincent njoroge <email@example.com>
2020-02-23 16:07:12 +0300

    it will add the assets

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+//what we do section //
+
+$(document).ready(function () {
+    $(".design_icon").click(function () {
+        $("#design-p").toggle();
+    });
+
+    $(".dev_icon").click(function () {
+        $("#development").toggle();
+    });
+    
+    $(".product_icon").click(function () {
+        $("#pmanagent").toggle();
+    });
+});
